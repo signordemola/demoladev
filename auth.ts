99f9f9cd756fe912bf2a7ceb955cd4c2be558825
signordemola <adedamola4678@gmail.com>
2025-03-31 07:54:32 -0500

    auth prisma

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import { getUserById } from "./lib/dal";
 
